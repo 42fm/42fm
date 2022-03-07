1bdf1a33bc5562312c0b3249f671c4f27450f2a7
loczek <30776250+loczek@users.noreply.github.com>
2022-03-07 20:47:51 +0100

    fix: change sockets uri to a string for firefox

--- src/socket.ts
+++ src/socket.ts
@@ -3,14 +3,14 @@ import { ClientToServerEvents, ServerToClientEvents } from "@typings/index";
 import { log } from "./utils/log";
 import { getSetting } from "./utils/settings";
 
-const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
-  process.env.SOCKET_ENDPOINT!,
-  {
-    withCredentials: true,
-    autoConnect: false,
-    transports: ["websocket"],
-  }
-);
+// must be string or else it wont work in firefox
+const URI = process.env.NODE_ENV === "production" ? "https://twitch-chat-music.herokuapp.com" : "http://localhost:5000";
+
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(URI, {
+  withCredentials: true,
+  autoConnect: false,
+  transports: ["websocket"],
+});
 
 getSetting("autoconnect") && socket.connect();
 
