ad00c74556b77bf8af4ba51921aaaa9286dd1647
loczek <30776250+loczek@users.noreply.github.com>
2026-03-05 10:40:53 +0100

    refactor: remove unused deps

--- src/pages/content/content.tsx
+++ src/pages/content/content.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import HeaderButton from "../../components/HeaderButton";
