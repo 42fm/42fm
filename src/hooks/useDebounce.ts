6018e5bbe1f1f315f3aa216bf115508f80600239
loczek <30776250+loczek@users.noreply.github.com>
2026-03-13 12:18:14 +0100

    refactor: remove unused import

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function useDebounce(value: any, delay: number | undefined) {
   const [debouncedValue, setDebouncedValue] = useState(value);
