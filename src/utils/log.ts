da1a783c1b6f02224a6384a914609c400f18d148
loczek <30776250+loczek@users.noreply.github.com>
2026-03-21 00:32:32 +0100

    refactor: add log level colors

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,18 +1,18 @@
 class Logger {
   public error(...data: any[]) {
-    console.error("%c[42FM]", "color: #FF7A00", `[error]`, data);
+    console.error(`%c42fm %c[error]:`, "color: #ff7a00", "color: #ef4444", ...data);
   }
 
   public warn(...data: any[]) {
-    console.warn("%c[42FM]", "color: #FF7A00", `[warn]`, data);
+    console.warn(`%c42fm %c[warn]:`, "color: #ff7a00", "color: #eab308", ...data);
   }
 
   public info(...data: any[]) {
-    console.log("%c[42FM]", "color: #FF7A00", `[info]`, data);
+    console.log(`%c42fm %c[info]:`, "color: #ff7a00", "color: #0ea5e9", ...data);
   }
 
   public debug(...data: any[]) {
-    console.debug("%c[42FM]", "color: #FF7A00", `[debug]`, data);
+    console.debug(`%c42fm %c[debug]:`, "color: #ff7a00", "color: #22c55e", ...data);
   }
 }
 
