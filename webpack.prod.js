e3ec623ee38cf00aa37af3062e26da9536a35e70
loczek <30776250+loczek@users.noreply.github.com>
2026-04-12 12:37:15 +0200

    build: add missing entrypoints

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,22 @@
+import CopyPlugin from "copy-webpack-plugin";
+import Dotenv from "dotenv-webpack";
+import { dirname, resolve } from "path";
+import TerserPlugin from "terser-webpack-plugin";
+import { fileURLToPath } from "url";
 import { merge } from "webpack-merge";
 import baseConfig from "./webpack.config.js";
-import Dotenv from "dotenv-webpack";
-import CopyPlugin from "copy-webpack-plugin";
-import TerserPlugin from "terser-webpack-plugin";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @returns {webpack.Configuration} */
 const config = () => {
   return {
     mode: "production",
+    entry: {
+      content: resolve(__dirname, "src", "pages", "content", "content.tsx"),
+      background: resolve(__dirname, "src", "pages", "background", "background.ts"),
+      iframe: resolve(__dirname, "src", "iframe.ts"),
+    },
     optimization: {
       minimize: true,
       minimizer: [
