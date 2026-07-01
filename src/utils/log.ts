d3b51abac56aeb76d883ac77f1d54ba59dab6e58
loczek <30776250+loczek@users.noreply.github.com>
2026-07-02 01:56:28 +0200

    refactor: add label option to logger

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,21 +1,25 @@
 class Logger {
+  private label?: string;
+
+  constructor(label?: string) {
+    this.label = label;
+  }
+
   public error(...data: any[]) {
-    console.error(`%c42fm %c[error]:`, "color: #ff7a00", "color: #ef4444", ...data);
+    console.error(`%c42fm %c[error]${this.label ? ` (${this.label})` : ""}:`, "color: #ff7a00", "color: #ef4444", ...data);
   }
 
   public warn(...data: any[]) {
-    console.warn(`%c42fm %c[warn]:`, "color: #ff7a00", "color: #eab308", ...data);
+    console.warn(`%c42fm %c[warn]${this.label ? ` (${this.label})` : ""}:`, "color: #ff7a00", "color: #eab308", ...data);
   }
 
   public info(...data: any[]) {
-    console.log(`%c42fm %c[info]:`, "color: #ff7a00", "color: #0ea5e9", ...data);
+    console.log(`%c42fm %c[info]${this.label ? ` (${this.label})` : ""}:`, "color: #ff7a00", "color: #0ea5e9", ...data);
   }
 
   public debug(...data: any[]) {
-    console.debug(`%c42fm %c[debug]:`, "color: #ff7a00", "color: #22c55e", ...data);
+    console.debug(`%c42fm %c[debug]${this.label ? ` (${this.label})` : ""}:`, "color: #ff7a00", "color: #22c55e", ...data);
   }
 }
 
-const logger = new Logger();
-
-export { logger };
+export { Logger };
