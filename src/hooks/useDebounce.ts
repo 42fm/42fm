83f10d5a984b77b97e13c9bb2dd29fd3604d652f
loczek <30776250+loczek@users.noreply.github.com>
2026-08-12 08:50:35 +0200

    refactor: make generic

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-function useDebounce(value: any, delay: number | undefined) {
+function useDebounce<T>(value: T, delay: number | undefined) {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
     const handler = setTimeout(() => {
