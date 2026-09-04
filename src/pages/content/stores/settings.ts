77311151589b12800e78e12585dd40ebdd0d69f7
loczek <30776250+loczek@users.noreply.github.com>
2026-09-04 23:40:11 +0200

    refactor: add settings store

--- src/pages/content/stores/settings.ts
+++ src/pages/content/stores/settings.ts
@@ -1,4 +1,6 @@
 import { create } from "zustand";
+import { createJSONStorage } from "zustand/middleware";
+import { persist } from "zustand/middleware";
 
 interface SettingsState {
   isOpen: boolean;
@@ -7,5 +9,45 @@ interface SettingsState {
 
 export const useSettingsState = create<SettingsState>((set) => ({
   isOpen: false,
-  setIsOpen: (isOpen) => set((_state) => ({ isOpen })),
+  setIsOpen: (isOpen) => set(() => ({ isOpen })),
 }));
+
+export interface SettingsWithActions extends SettingsValuesPartial {
+  setSetting: <T extends keyof SettingsValues>(key: T, value: SettingsValues[T]) => void;
+  removeSetting: <T extends keyof SettingsValues>(key: T) => void;
+}
+
+type SettingsValuesPartial = Partial<SettingsValues>;
+
+export interface SettingsValues {
+  position: "top" | "center" | "bottom";
+  autoConnect: boolean;
+  isExpanded: boolean;
+  hideLeaderboard: boolean;
+  disableBadges: boolean;
+  disablePaints: boolean;
+  hideProgress: boolean;
+}
+
+export const default_settings: SettingsValues = {
+  position: "top",
+  autoConnect: true,
+  isExpanded: false,
+  hideLeaderboard: true,
+  disableBadges: false,
+  disablePaints: false,
+  hideProgress: true,
+};
+
+export const useSettingsStore = create<SettingsWithActions>()(
+  persist(
+    (set) => ({
+      setSetting: (key, value) => set(() => ({ [key]: value })),
+      removeSetting: (key) => set(() => ({ [key]: undefined })),
+    }),
+    {
+      name: "42fm:settings",
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
+);
