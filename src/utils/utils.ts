9cc4e7294c3c5119bc3fdb0b208dfcbc0244f960
loczek <30776250+loczek@users.noreply.github.com>
2026-09-06 23:56:51 +0200

    fix: only display ones place for minutes

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,6 +6,7 @@ export const distanceFormatHMS = (current: Duration, total: Duration) => {
 
 function durationToString(duration: Duration): string {
   return [duration.hours, duration.minutes ?? 0, duration.seconds ?? 0]
+    .map((x, i) => x?.toString().padStart(i, "0"))
     .filter((x) => x !== undefined)
     .map((x) => x?.toString().padStart(2, "0"))
     .join(":");
