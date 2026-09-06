499dd6ccb4243e11aded7a425694e2f1050361f0
loczek <30776250+loczek@users.noreply.github.com>
2026-09-06 23:58:49 +0200

    refactor: remove duplicate map

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -8,6 +8,5 @@ function durationToString(duration: Duration): string {
   return [duration.hours, duration.minutes ?? 0, duration.seconds ?? 0]
     .map((x, i) => x?.toString().padStart(i, "0"))
     .filter((x) => x !== undefined)
-    .map((x) => x?.toString().padStart(2, "0"))
     .join(":");
 }
